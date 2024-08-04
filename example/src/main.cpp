b18c67a2aa3f17af90400cb7cd4f4b486f5f3af8
Dmitriy <super.dima771@yandex.ru>
2024-08-04 12:43:34 +0300

    Better example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,25 +1,93 @@
-//
-// Created by super on 30.07.2024.
-//
+// GLEW
+#include <GL/glew.h>
 
-#include <iostream>
-#include "Render.h"
-#include "Audio.h"
+// GLFW
+#include <GLFW/glfw3.h>
+
+// OPENAL
+#include <alc.h>
+
+//LOG
 #include "LOG.h"
+#ifdef _DEBUG
 #include "spdlog/sinks/stdout_color_sinks.h"
+#else
+#include "spdlog/sinks/basic_file_sink.h"
+#endif
+//STB
+#include <stb_image.h>
+//MEMORY
+#include "memory"
+//OTHER
+#include "System/SystemAdapter.h"
+//ARCHIVE
+#include "System/ZipArchive.h"
+//OTHER
 #include "debug.h"
+#include "System/Localization.h"
 #include "Font.h"
-#include "SystemAdapter.h"
+#include "MergedRender.h"
+
+#ifdef _WINDOWS
+#include <windows.h>
+#endif
+#include <memory>
+std::unique_ptr<Font::Font> baseFont = nullptr;
+const std::unique_ptr<Font::Font>& getFont(){return baseFont;}
+#ifndef DO_WINMAIN
+bool skipintro = false;
+bool genlangfile = false;
+#endif
+//GAME
+std::unique_ptr<MergedRender> r {new MergedRender()};
+
 
 void window_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void error_callback(int code, const char *description);
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
+// Window dimensions
 GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint& getWidth()
+{
+    return WIDTH;
+}
+const GLuint& getHeight()
+{
+    return HEIGHT;
+}
 GLFWwindow* window;
 ALCdevice *Aldevice;
 ALCcontext *AlContext;
-int main(){
+const GLFWwindow* const getwindow()
+{
+    return window;
+}
+void Init()
+{
+    SPDLOG_DEBUG("loading Assets.zip && font.ttf");
+    auto zzz = "res.zip";
+    if (std::filesystem::is_regular_file(zzz)) {
+        ZipArchive zip{zzz};
+        char *content = NULL; zip_uint64_t size = 0;
+        zip.get("font.ttf", content, size);
+        if (content != NULL) {
+            baseFont = std::make_unique<Font::Font>(reinterpret_cast<const FT_Byte *>(content), size);
+            SPDLOG_INFO("font.ttf loaded && applied");
+        } else {
+            SPDLOG_WARN("font.ttf not found");
+        }
+    } else {
+        SPDLOG_WARN("res.zip not found");
+    }
+}
+void Domain(){
+    //Create Log
+#ifdef _DEBUG
+// NO abort() message in windows
+    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
     try
     {
         // create a color multi-threaded logger
@@ -31,7 +99,11 @@ int main(){
     {
         std::cout << "Log init failed: " << ex.what() << std::endl;
     }
-
+#else
+    auto logger = spdlog::basic_logger_mt("logger", "logs/latest.log");
+//    spdlog::set_level(spdlog::level::info); // Set global log level to debug3
+    spdlog::set_default_logger(logger);
+#endif
     spdlog::set_pattern("%s:%# [%^%l%$] %v");
     SPDLOG_INFO("Starting...");
     SPDLOG_INFO("{} by {} v.{}", APPNAME, APPAUTHOR, APPVERSION);
@@ -64,24 +136,57 @@ int main(){
     glfwSetWindowSizeCallback(window, window_size_callback);
     glfwSetWindowSizeLimits(window, 400,400, GLFW_DONT_CARE, GLFW_DONT_CARE);
     glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
 
     //Init
     SystemAdapter::Init();
+    Localization::init("lang.lang");
+#ifdef _DEBUG
+    if (genlangfile) {
+        Localization::writefile();
+    }
+#endif
     //INIT FONT LIB
     Font::InitLib();
     // Init OpenAl
     Aldevice = alcOpenDevice(nullptr);
     AlContext = alcCreateContext(Aldevice, nullptr);
     alcMakeContextCurrent(AlContext);
+    Init();
+#if defined(_DEBUG) && !defined(DO_WINMAIN)
+    if (skipintro) {
+//        std::thread t{Init};
+//        if (t.joinable()) {
+//            t.join();
+//        }
+        SPDLOG_DEBUG("intro_skipped");
+    } else {
+    }
+#else
+    setContext(new IntroContext());
+#endif
     initEngine();
-    while (!glfwWindowShouldClose(window)) {
+    // INIT GAME
+
+    r->setSpeed(MergedRender::SpeedContent::STATIC);
+    r->quard.reset(new ExtendedQuard({-.5,-.5},1,1));
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
         START_LOOP
 
-
+        r->use({1});
 
         END_LOOP(window)
     }
     SPDLOG_DEBUG("Closing...");
+#ifdef _DEBUG
+    if (genlangfile) {
+        Localization::genfile("lang.lang");
+    }
+#endif
     //OPENAL
     alcMakeContextCurrent(nullptr);
     alcDestroyContext(AlContext);
@@ -89,24 +194,65 @@ int main(){
     // O
     SystemAdapter::Destroy();
     glfwTerminate();
-    return 0;
 }
-void error_callback(int code, const char *description)
+// The MAIN function, from here we start the application and run the game loop
+#ifdef DO_WINMAIN
+int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+#else
+int main(int argc, char** argv)
+#endif
 {
-    SPDLOG_ERROR("GLFW error was occuruped" + std::string(description) + " with code" + std::to_string(code));
+#ifndef DO_WINMAIN
+    for (int i = 0; i < argc; ++i) {
+        if (std::string(argv[i]) == "skipintro")
+        {
+            skipintro = true;
+        } else if (std::string(argv[i]) == "genlangfile") {
+            genlangfile = true;
+        }
+    }
+    delete argv;
+#endif
+    try {
+        Domain();
+    } catch (const std::exception& e)
+    {
+        SPDLOG_CRITICAL("GLOBAL CRITICAL ERROR!!!! {}", e.what());
+        return -1;
+    } catch (...)
+    {
+        SPDLOG_CRITICAL("GLOBAL CRITICAL ERROR!!!!");
+        return -2;
+    }
+    return 0;
 }
 void window_size_callback(GLFWwindow* window, int width, int height){
     WIDTH = width; HEIGHT = height;
-//    currentContext->size_callback(width, height);
+    glViewport(0,0,width,height);
 }
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (action == GLFW_PRESS)
+}
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+}
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+}
+void error_callback(int code, const char *description)
+{
+    SPDLOG_ERROR("GLFW error was occurred {} with code {}", description, code);
+}
+bool fullscreen = false;
+void SwitchFullscreen(){
+    if (fullscreen)
     {
-        switch (key) {
-            case GLFW_KEY_ESCAPE:
-                glfwSetWindowShouldClose(window, GLFW_TRUE);
-                break;
-        }
+        glfwSetWindowMonitor(window, NULL, 100, 100, 800, 600, NULL);
+        fullscreen = false;
+    } else {
+        const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+        glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, mode->width, mode->height, mode->refreshRate);
+        fullscreen = true;
     }
 }
